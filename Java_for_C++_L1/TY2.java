4c665fba77891910885f9b400be885b32cb580e0
Harry <56703226+Harry-Lukasz@users.noreply.github.com>
2021-07-30 12:50:31 +0930

    Add files via upload

---
+++ Java_for_C++_L1/TY2.java
@@ -0,0 +1,13 @@
+	class TY2 {
+		public static void main( String [] args )
+		{
+		    int sum = 0 ;
+
+		    for(int i = 0; i <10; i++)
+		    {
+		  		sum += i ;
+		    }
+
+		    System.out.println("The sum is: " + sum) ;
+		}
+	}
